da620e3f45611fc581e8f0bfcf11e4ca717b95d6
Benjamín <56449637+neopkr@users.noreply.github.com>
2021-07-25 15:42:39 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+std::string key = "VK7JG-NPHTM-C97JM-9MPGT-3V66T";
+std::string command = "slmgr.vps -ipk " + key;
+
+int main()
+{
+    cout << "Activating Windows 10 Pro..." << endl;
+    system(command.c_str());
+    system("pause");
+    return 0;
+}
